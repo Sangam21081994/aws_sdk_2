dda903a8d77ad4533132080cb94058bb667f784f
Sangamnath Ingalalli <sangamnath.professional@gmail.com>
2025-05-28 23:58:51 +0530

    add of the cucmber file

--- src/test/java/com/example/runners/CucumberTestRunner.java
+++ src/test/java/com/example/runners/CucumberTestRunner.java
@@ -1,6 +1,6 @@
 package com.example.runners;
 
-import io.cucumber.junit.Cucumber;
+import cucumber.api.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
